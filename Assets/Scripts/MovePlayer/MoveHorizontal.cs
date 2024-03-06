af242be95c33dd970206157ae286b25773182e60
ptaillandier <patrick.taillandier@gmail.com>
2024-03-06 15:12:11 +0700

    fixes issue with light and moving

--- Assets/Scripts/MovePlayer/MoveHorizontal.cs
+++ Assets/Scripts/MovePlayer/MoveHorizontal.cs
@@ -1,31 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.InputSystem;
+using UnityEngine.XR;
+
 
 
 
 public class MoveHorizontal : InputData
 {
-    
+    public bool RightHand = true;
+
     [SerializeField] private float speed = 2.0f;
     [SerializeField] private float speedRotation = 10.0f;
     [SerializeField] private bool Strafe = false;
-    [SerializeField] private InputActionReference stick;
+    public InputHelpers.Axis2D stick = InputHelpers.Axis2D.PrimaryAxis2D;
 
     // ############################################################
-   
+
     private void FixedUpdate()
     {
-        if (SimulationManager.Instance.IsGameState(GameState.GAME)) MoveHorizontally();
+        if (SimulationManager.Instance.IsGameState(GameState.GAME))
+            MoveHorizontally();
     }
 
     // ############################################################
 
-    private void MoveHorizontally() {
-        Vector2 val = stick.action.ReadValue<Vector2>();
+    private void MoveHorizontally()
+    {
+        InputDevice hand = RightHand ? _rightController : _leftController;
+        Vector2 val;
+        hand.TryReadAxis2DValue(stick, out val);
         Vector3 vectF = Camera.main.transform.forward;
         vectF.y = 0;
         vectF = Vector3.Normalize(vectF);
@@ -39,10 +42,11 @@ public class MoveHorizontal : InputData
             vectR = Vector3.Normalize(vectR);
 
             transform.position += (vectR * speed * Time.fixedDeltaTime * val.x);
-        } else
+        }
+        else
         {
 
-           transform.Rotate(new Vector3(0,1,0), Time.fixedDeltaTime * speedRotation * val.x);
+            transform.Rotate(new Vector3(0, 1, 0), Time.fixedDeltaTime * speedRotation * val.x);
         }
     }
 }
